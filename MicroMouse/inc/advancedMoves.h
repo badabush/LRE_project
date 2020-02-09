92558c3218c6c8a0332e0584a1f7a7154aeb6cdd
willinum <38470028+willinum@users.noreply.github.com>
2020-02-09 22:40:49 +0100

    Create advancedMoves.h

---
+++ MicroMouse/inc/advancedMoves.h
@@ -0,0 +1,30 @@
+/*
+ * advancedMoves.h
+ *
+ *  Created on: 09.02.2020
+ *      Author: Willi
+ */
+
+extern int filt_L = 0;
+extern int filt_C = 0;
+extern int filt_R = 0;
+
+int dummy;
+int cellDist; // length of a cell
+
+int xCmin= 4; // min distance Center
+int xRmin= 15; // min distance R
+int xLmin= 15; // min distance L
+int xClive; // live dist C
+int xRlive; // live dist R
+int xLlive; // live dist L
+int xCold; // old dist C
+int xRold; // old dist R
+int xLold; // old dist L
+int xLdiff; // difference between current dist to lWall and dist to lWall in previous state
+
+float reqTurn; // required turning angle
+
+
+int desiredDist; // dist that mouse has to move forward
+int movedDist; // dist that mouse has moved forward
